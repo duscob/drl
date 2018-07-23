7f85b46b150513ea6debd244577c6d12b39f3fc5
Dustin Cobas Batista <dustin.cobas@gmail.com>
2018-07-23 18:45:35 -0400

    Add Precomputed Document List On Document Array
    This is a new document listing algorithm based on grammar compression of the document array. This is a very primary version.

---
+++ include/drl/pdloda.h
@@ -0,0 +1,111 @@
+//
+// Created by Dustin Cobas Batista <dustin.cobas@gmail.com> on 7/20/18.
+//
+
+#ifndef DRL_PDLODA_H
+#define DRL_PDLODA_H
+
+#include <vector>
+
+#include <grammar/re_pair.h>
+#include <grammar/slp_helper.h>
+
+#include "construct_da.h"
+
+
+namespace drl {
+
+//! Precomputed Document List On Document Array
+/**
+ *
+ * @tparam _SA Suffix Array
+ * @tparam _SLP Straight-Line Program
+ */
+template<typename _SA, typename _SLP>
+class PDLODA {
+ public:
+  template<typename _E = int8_t>
+  PDLODA(const std::string &filename, const _E &doc_delim) {
+    uint8_t num_bytes = sizeof(_E);
+    std::string id = sdsl::util::basename(filename) + "_";
+    sdsl::cache_config cconfig{false, "./", id};
+    construct(sa_, filename, cconfig, num_bytes);
+
+    {
+      sdsl::bit_vector doc_border;
+
+      {
+        std::ifstream in(filename, std::ios::binary);
+        drl::ConstructDocBorder(std::istream_iterator<_E>(in),
+                                std::istream_iterator<_E>{},
+                                doc_border,
+                                doc_delim,
+                                sa_.size());
+      }
+      sdsl::bit_vector::rank_1_type b_rank(&doc_border);
+
+      sdsl::int_vector<> doc_array(sa_.size() + 1);
+      drl::ConstructDocArray(sa_, b_rank, doc_array);
+
+      grammar::RePairEncoder<true> encoder;
+      ComputeSLP(doc_array.begin(), doc_array.end(), encoder, slp_);
+    }
+  }
+
+  template<typename _OI>
+  void SearchInRange(std::size_t begin, std::size_t end, _OI out) {
+    std::vector<std::size_t> span_cover;
+    ComputeSpanCover(slp_, begin, end, back_inserter(span_cover));
+
+    if (span_cover.empty())
+      return;
+
+    auto docs = slp_.GetData(span_cover[0]);
+    decltype(docs) tmp_docs;
+//    for (int i = 1; i < span_cover.size(); ++i) {
+//      tmp_docs.swap(docs);
+//      docs.clear();
+//      const auto &partial_docs = slp_.GetData(span_cover[i]);
+//      set_union(tmp_docs.begin(), tmp_docs.end(), partial_docs.begin(), partial_docs.end(), back_inserter(docs));
+//    }
+    for (int i = 1; i < span_cover.size(); ++i) {
+      const auto &partial_docs = slp_.GetData(span_cover[i]);
+
+      tmp_docs.resize(docs.size() + partial_docs.size());
+      tmp_docs.swap(docs);
+
+      auto it = set_union(tmp_docs.begin(), tmp_docs.end(), partial_docs.begin(), partial_docs.end(), docs.begin());
+
+      docs.resize(it - docs.begin());
+    }
+
+
+//    std::set<std::size_t> docs;
+//    for (auto &&v : span_cover) {
+//      const auto &partial_docs = slp_.GetData(v);
+//      docs.insert(partial_docs.begin(), partial_docs.end());
+////    std::copy(partial_docs.begin(), partial_docs.end(), out);
+//    }
+
+    std::copy(docs.begin(), docs.end(), out);
+  }
+
+
+  template<typename _II, typename _OI>
+  void Search(_II begin, _II end, _OI out) {
+    std::size_t sp = 1, ep = 0;
+    if (0 == backward_search(sa_, 0, sa_.size() - 1, begin, end, sp, ep))
+      return;
+
+    SearchInRange(sp, ep + 1, out);
+  }
+
+
+ private:
+  _SA sa_;
+  _SLP slp_{0};
+};
+
+}
+
+#endif //DRL_PDLODA_H
---
+++ test/pdloda_test.cpp
@@ -0,0 +1,96 @@
+//
+// Created by Dustin Cobas Batista <dustin.cobas@gmail.com> on 6/2/2018.
+//
+
+
+#include <gtest/gtest.h>
+
+#include <sdsl/csa_wt.hpp>
+#include <sdsl/csa_bitcompressed.hpp>
+#include <sdsl/suffix_array_algorithm.hpp>
+
+#include <grammar/slp.h>
+#include <grammar/slp_metadata.h>
+
+#include "drl/pdloda.h"
+
+
+class PDLODASearch_TF : public ::testing::TestWithParam<std::tuple<std::string, std::vector<uint32_t>>> {
+ public:
+  static std::string tmp_filename;
+
+  static void SetUpTestCase() {
+    tmp_filename = std::tmpnam(nullptr);
+
+    std::ofstream out(tmp_filename, std::ios::binary);
+    out << "TATA$LATA$AAAA$";
+  }
+
+  static void TearDownTestCase() {
+    std::remove(tmp_filename.c_str());
+  }
+};
+std::string PDLODASearch_TF::tmp_filename;
+
+
+TEST_P(PDLODASearch_TF, search) {
+  drl::PDLODA<sdsl::csa_wt<>, grammar::SLPWithMetadata<grammar::PTS>> idx(tmp_filename, '$');
+
+  const auto &pattern = std::get<0>(GetParam());
+  const auto &eresult = std::get<1>(GetParam());
+
+  std::vector<uint32_t> result;
+  idx.Search(pattern.begin(), pattern.end(), std::back_inserter(result));
+  EXPECT_EQ(result, eresult);
+}
+
+
+INSTANTIATE_TEST_CASE_P(
+    PDLODA,
+    PDLODASearch_TF,
+    ::testing::Values(
+        std::make_tuple("TA", std::vector<uint32_t>{1, 2}),
+        std::make_tuple("A", std::vector<uint32_t>{1, 2, 3}),
+        std::make_tuple("LT", std::vector<uint32_t>{}),
+        std::make_tuple("AA", std::vector<uint32_t>{3}),
+        std::make_tuple("TATA", std::vector<uint32_t>{1})
+    )
+);
+
+
+template<typename T>
+class PDLODAGeneric_TF : public ::testing::Test {
+ public:
+  static std::string tmp_filename;
+
+  static void SetUpTestCase() {
+    tmp_filename = std::tmpnam(nullptr);
+
+    std::ofstream out(tmp_filename, std::ios::binary);
+    out << "TATA$LATA$AAAA$";
+  }
+
+  static void TearDownTestCase() {
+    std::remove(tmp_filename.c_str());
+  }
+};
+template <typename T>
+std::string PDLODAGeneric_TF<T>::tmp_filename;
+
+
+using MyTypes = ::testing::Types<
+    drl::PDLODA<sdsl::csa_wt<>, grammar::SLPWithMetadata<grammar::PTS>>,
+    drl::PDLODA<sdsl::csa_bitcompressed<>, grammar::SLPWithMetadata<grammar::PTS>>>;
+TYPED_TEST_CASE(PDLODAGeneric_TF, MyTypes);
+
+
+TYPED_TEST(PDLODAGeneric_TF, search) {
+  TypeParam idx(this->tmp_filename, '$');
+
+  std::string pattern = "TA";
+  std::vector<uint32_t> eresult = {1, 2};
+
+  std::vector<uint32_t> result;
+  idx.Search(pattern.begin(), pattern.end(), std::back_inserter(result));
+  EXPECT_EQ(result, eresult);
+}
