d55812a30e170632dba4cbe3d0061dafd2392511
Dustin Cobas <dustin.cobas@gmail.com>
2019-09-16 11:12:36 -0300

    Add RLSLP-based index to build tool

--- tool/build_dl_idx.cpp
+++ tool/build_dl_idx.cpp
@@ -15,11 +15,134 @@
 
 #include "drl/io.h"
 #include "drl/sa.h"
+#include "drl/rle.h"
 
 
 DEFINE_string(data, "", "Collection file.");
 DEFINE_int32(bs, 512, "Block size.");
 DEFINE_int32(sf, 4, "Storing factor.");
+
+using drl::Load;
+using drl::Save;
+
+auto BM_BuildSLPIndex = [](auto first,
+                           auto last,
+                           auto cconfig_sep_0,
+                           const auto &slp_filename_prefix,
+                           const auto &pts_filename_prefix) {
+  grammar::RePairEncoder<true> encoder;
+
+  grammar::SLP<> slp;
+  if (!Load(slp, slp_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct SLP" << std::endl;
+
+    auto wrapper = BuildSLPWrapper(slp);
+    encoder.Encode(first, last, wrapper);
+
+    Save(slp, slp_filename_prefix, cconfig_sep_0);
+  }
+//  const auto kSize_slp = sdsl::size_in_bytes(slp);
+
+  auto prefix = std::to_string(FLAGS_bs) + "-" + std::to_string(FLAGS_sf);
+  auto sslp_filename_prefix = prefix + "-" + slp_filename_prefix;
+  auto spts_filename_prefix = prefix + "-" + pts_filename_prefix;
+
+  grammar::CombinedSLP<> cslp;
+  grammar::Chunks<> cslp_chunks;
+  if (!Load(cslp, sslp_filename_prefix, cconfig_sep_0) || !Load(cslp_chunks, spts_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct CSLP && CSLPChunks" << std::endl;
+
+    auto wrapper = BuildSLPWrapper(cslp);
+    encoder.Encode(first, last, wrapper);
+
+//    auto span_to_big = [](const auto &_set, const auto &_lchildren, const auto &_rchildren) -> bool {
+//      return 1000 <= _lchildren.size() + _rchildren.size();
+//    };
+
+    grammar::AddSet<decltype(cslp_chunks)> add_set(cslp_chunks);
+//    cslp.Compute(256, add_set, add_set, grammar::MustBeSampled<decltype(cslp_chunks)>(cslp_chunks, 16/*, rlcsa->getNumberOfSequences()*/));
+    cslp.Compute(FLAGS_bs, add_set, add_set, grammar::MustBeSampled<decltype(cslp_chunks)>(
+        grammar::AreChildrenTooBig<decltype(cslp_chunks)>(cslp_chunks, FLAGS_sf)/*,
+        grammar::IsEqual<decltype(cslp_chunks)>(cslp_chunks, 100),
+        span_to_big*/));
+
+    Save(cslp, sslp_filename_prefix, cconfig_sep_0);
+    Save(cslp_chunks, spts_filename_prefix, cconfig_sep_0);
+  }
+//  const auto kSize_cslp = sdsl::size_in_bytes(cslp);
+
+  auto bit_compress = [](sdsl::int_vector<> &_v) { sdsl::util::bit_compress(_v); };
+  grammar::RePairEncoder<false> encoder_nslp;
+  grammar::LightSLP<> lslp;
+  if (!Load(lslp, sslp_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct LSLP" << std::endl;
+
+    lslp.Compute(first, last, encoder_nslp, cslp);
+
+    Save(lslp, sslp_filename_prefix, cconfig_sep_0);
+  }
+//  const auto kSize_lslp = sdsl::size_in_bytes(lslp);
+
+  grammar::LightSLP<grammar::BasicSLP<sdsl::int_vector<>>,
+                    grammar::SampledSLP<>,
+                    grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>> lslp_bslp;
+  if (!Load(lslp_bslp, sslp_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct LBSLP" << std::endl;
+
+    lslp_bslp = decltype(lslp_bslp)(lslp, bit_compress, bit_compress, bit_compress, bit_compress);
+
+    Save(lslp_bslp, sslp_filename_prefix, cconfig_sep_0);
+  }
+//  const auto kSize_lslp_bslp = sdsl::size_in_bytes(lslp_bslp);
+
+  grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>> sslp_chunks_bc;
+  if (!Load(sslp_chunks_bc, spts_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct Chunks<BC>" << std::endl;
+
+    sslp_chunks_bc = decltype(sslp_chunks_bc)(cslp_chunks, bit_compress, bit_compress);
+
+    Save(sslp_chunks_bc, spts_filename_prefix, cconfig_sep_0);
+  }
+
+  grammar::GCChunks<grammar::SLP<>> sslp_gcchunks;
+  if (!Load(sslp_gcchunks, spts_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct GCChunks" << std::endl;
+
+    const auto &objs = cslp_chunks.GetObjects();
+    sslp_gcchunks.Compute(objs.begin(), objs.end(), cslp_chunks, encoder_nslp);
+
+    Save(sslp_gcchunks, spts_filename_prefix, cconfig_sep_0);
+  }
+//  const auto kSize_sslp_gcchunks = sdsl::size_in_bytes(sslp_gcchunks);
+
+  grammar::GCChunks<
+      grammar::SLP<sdsl::int_vector<>, sdsl::int_vector<>>,
+      true,
+      grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>> sslp_gcchunks_bc;
+  if (!Load(sslp_gcchunks_bc, spts_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct GCChunks<BC>" << std::endl;
+
+    sslp_gcchunks_bc =
+        decltype(sslp_gcchunks_bc)(sslp_gcchunks, bit_compress, bit_compress, bit_compress, bit_compress);
+
+    Save(sslp_gcchunks_bc, spts_filename_prefix, cconfig_sep_0);
+  }
+//  const auto kSize_sslp_gcchunks_bc = sdsl::size_in_bytes(sslp_gcchunks_bc);
+
+  grammar::GCChunks<
+      grammar::BasicSLP<sdsl::int_vector<>>,
+      true,
+      grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>> sslp_gcchunks_bslp_bc;
+  if (!Load(sslp_gcchunks_bslp_bc, spts_filename_prefix, cconfig_sep_0)) {
+    std::cout << "Construct GCChunks<BSLP,BC>" << std::endl;
+
+    sslp_gcchunks_bslp_bc =
+        decltype(sslp_gcchunks_bslp_bc)(sslp_gcchunks, bit_compress, bit_compress, bit_compress, bit_compress);
+
+    Save(sslp_gcchunks_bslp_bc, spts_filename_prefix, cconfig_sep_0);
+  }
+//  const auto kSize_sslp_gcchunks_bslp_bc = sdsl::size_in_bytes(sslp_gcchunks_bslp_bc);
+};
 
 
 int main(int argc, char *argv[]) {
@@ -54,7 +177,7 @@ int main(int argc, char *argv[]) {
   rlcsa->printInfo();
   rlcsa->reportSize(true);
 
-  const auto kNDocs = rlcsa->getNumberOfSequences();
+//  const auto kNDocs = rlcsa->getNumberOfSequences();
 
 
   // DA
@@ -68,133 +191,49 @@ int main(int argc, char *argv[]) {
     grammar::Construct(da, doc_array);
     sdsl::util::bit_compress(da);
   }
-  const auto kSize_da = sdsl::size_in_bytes(da);
+//  const auto kSize_da = sdsl::size_in_bytes(da);
 
 
   //SLP
-  grammar::RePairEncoder<true> encoder;
-
-  grammar::SLP<> slp;
-
-  using drl::Load;
-  using drl::Save;
-
-  if (!Load(slp, "slp", cconfig_sep_0)) {
-    std::cout << "Construct SLP" << std::endl;
-
-    auto wrapper = BuildSLPWrapper(slp);
-    encoder.Encode(da.begin(), da.end(), wrapper);
-
-    Save(slp, "slp", cconfig_sep_0);
-  }
-  const auto kSize_slp = sdsl::size_in_bytes(slp);
+  BM_BuildSLPIndex(da.begin(), da.end(), cconfig_sep_0, "slp", "pts");
 
 
-  auto prefix = std::to_string(FLAGS_bs) + "-" + std::to_string(FLAGS_sf);
-  auto slp_filename_prefix = prefix + "-slp";
-  auto pts_filename_prefix = prefix + "-pts";
+  // RLSLP
+  drl::RLEncoding<sdsl::bit_vector> rle_bv;
+  if (!Load(rle_bv, "rle-bv", cconfig_sep_0)) {
+    std::cout << "Construct Run-Length Encoding<bit_vector>" << std::endl;
 
-  grammar::CombinedSLP<> cslp;
-  grammar::Chunks<> cslp_chunks;
-  if (!Load(cslp, slp_filename_prefix, cconfig_sep_0) || !Load(cslp_chunks, pts_filename_prefix, cconfig_sep_0)) {
-    std::cout << "Construct CSLP && CSLPChunks" << std::endl;
+    sdsl::bit_vector tmp_bv_runs;
+    drl::BuildRLEncoding(da.begin(), da.end(), tmp_bv_runs);
 
-    auto wrapper = BuildSLPWrapper(cslp);
-    encoder.Encode(da.begin(), da.end(), wrapper);
+    rle_bv = decltype(rle_bv)(tmp_bv_runs);
 
-    auto span_to_big = [](const auto &_set, const auto &_lchildren, const auto &_rchildren) -> bool {
-      return 1000 <= _lchildren.size() + _rchildren.size();
-    };
-
-    grammar::AddSet<decltype(cslp_chunks)> add_set(cslp_chunks);
-//    cslp.Compute(256, add_set, add_set, grammar::MustBeSampled<decltype(cslp_chunks)>(cslp_chunks, 16/*, rlcsa->getNumberOfSequences()*/));
-    cslp.Compute(FLAGS_bs, add_set, add_set, grammar::MustBeSampled<decltype(cslp_chunks)>(
-        grammar::AreChildrenTooBig<decltype(cslp_chunks)>(cslp_chunks, FLAGS_sf)/*,
-        grammar::IsEqual<decltype(cslp_chunks)>(cslp_chunks, 100),
-        span_to_big*/));
-
-    Save(cslp, slp_filename_prefix, cconfig_sep_0);
-    Save(cslp_chunks, pts_filename_prefix, cconfig_sep_0);
-  }
-  const auto kSize_cslp = sdsl::size_in_bytes(cslp);
-
-  auto bit_compress = [](sdsl::int_vector<> &_v) { sdsl::util::bit_compress(_v); };
-  grammar::RePairEncoder<false> encoder_nslp;
-  grammar::LightSLP<> lslp;
-  if (!Load(lslp, slp_filename_prefix, cconfig_sep_0)) {
-    std::cout << "Construct LSLP" << std::endl;
-
-    lslp.Compute(da.begin(), da.end(), encoder_nslp, cslp);
-
-    Save(lslp, slp_filename_prefix, cconfig_sep_0);
-  }
-  const auto kSize_lslp = sdsl::size_in_bytes(lslp);
-
-
-  grammar::LightSLP<grammar::BasicSLP<sdsl::int_vector<>>,
-                    grammar::SampledSLP<>,
-                    grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>> lslp_bslp;
-  if (!Load(lslp_bslp, slp_filename_prefix, cconfig_sep_0)) {
-    std::cout << "Construct LBSLP" << std::endl;
-
-    lslp_bslp = decltype(lslp_bslp)(lslp, bit_compress, bit_compress, bit_compress, bit_compress);
-
-    Save(lslp_bslp, slp_filename_prefix, cconfig_sep_0);
-  }
-  const auto kSize_lslp_bslp = sdsl::size_in_bytes(lslp_bslp);
-
-
-  grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>> sslp_chunks_bc;
-  if (!Load(sslp_chunks_bc, pts_filename_prefix, cconfig_sep_0)) {
-    std::cout << "Construct Chunks<BC>" << std::endl;
-
-    sslp_chunks_bc = decltype(sslp_chunks_bc)(cslp_chunks, bit_compress, bit_compress);
-
-    Save(sslp_chunks_bc, pts_filename_prefix, cconfig_sep_0);
+    Save(rle_bv, "rle-bv", cconfig_sep_0);
   }
 
+  drl::RLEncoding<sdsl::sd_vector<>> rle_sdv;
+  if (!Load(rle_sdv, "rle-sdv", cconfig_sep_0)) {
+    std::cout << "Construct Run-Length Encoding<sd_vector>" << std::endl;
 
-  grammar::GCChunks<grammar::SLP<>> sslp_gcchunks;
-  if (!Load(sslp_gcchunks, pts_filename_prefix, cconfig_sep_0)) {
-    std::cout << "Construct GCChunks" << std::endl;
+    rle_sdv = decltype(rle_sdv)(rle_bv);
 
-    const auto &objs = cslp_chunks.GetObjects();
-    sslp_gcchunks.Compute(objs.begin(), objs.end(), cslp_chunks, encoder_nslp);
-
-    Save(sslp_gcchunks, pts_filename_prefix, cconfig_sep_0);
+    Save(rle_sdv, "rle-sdv", cconfig_sep_0);
   }
-  const auto kSize_sslp_gcchunks = sdsl::size_in_bytes(sslp_gcchunks);
 
+  drl::RLEncoding<sdsl::rrr_vector<>> rle_rrrv;
+  if (!Load(rle_rrrv, "rle-rrrv", cconfig_sep_0)) {
+    std::cout << "Construct Run-Length Encoding<rrr_vector>" << std::endl;
 
-  grammar::GCChunks<
-      grammar::SLP<sdsl::int_vector<>, sdsl::int_vector<>>,
-      true,
-      grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>> sslp_gcchunks_bc;
-  if (!Load(sslp_gcchunks_bc, pts_filename_prefix, cconfig_sep_0)) {
-    std::cout << "Construct GCChunks<BC>" << std::endl;
+    rle_rrrv = decltype(rle_rrrv)(rle_bv);
 
-    sslp_gcchunks_bc =
-        decltype(sslp_gcchunks_bc)(sslp_gcchunks, bit_compress, bit_compress, bit_compress, bit_compress);
-
-    Save(sslp_gcchunks_bc, pts_filename_prefix, cconfig_sep_0);
+    Save(rle_rrrv, "rle-rrrv", cconfig_sep_0);
   }
-  const auto kSize_sslp_gcchunks_bc = sdsl::size_in_bytes(sslp_gcchunks_bc);
 
-
-  grammar::GCChunks<
-      grammar::BasicSLP<sdsl::int_vector<>>,
-      true,
-      grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>> sslp_gcchunks_bslp_bc;
-  if (!Load(sslp_gcchunks_bslp_bc, pts_filename_prefix, cconfig_sep_0)) {
-    std::cout << "Construct GCChunks<BSLP,BC>" << std::endl;
-
-    sslp_gcchunks_bslp_bc =
-        decltype(sslp_gcchunks_bslp_bc)(sslp_gcchunks, bit_compress, bit_compress, bit_compress, bit_compress);
-
-    Save(sslp_gcchunks_bslp_bc, pts_filename_prefix, cconfig_sep_0);
-  }
-  const auto kSize_sslp_gcchunks_bslp_bc = sdsl::size_in_bytes(sslp_gcchunks_bslp_bc);
-
+  BM_BuildSLPIndex(drl::RLEIterator<decltype(da)::iterator>(da.begin(), da.end()),
+                   drl::RLEIterator<decltype(da)::iterator>(da.end(), da.end()),
+                   cconfig_sep_0,
+                   "rlslp",
+                   "rlpts");
 
   return 0;
 }
